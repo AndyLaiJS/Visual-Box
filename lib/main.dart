012853d2e8e595953ccfb7240e76fca2f0226be4
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-06-22 16:00:24 +0800

    pubspec: Add Sticky Headers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 void main() {
   runApp(MyApp());
